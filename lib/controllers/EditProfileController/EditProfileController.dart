3dd499033717e61309cbd91f14e167c06095954b
YashC-oder <yashmehra914@gmail.com>
2024-10-21 20:38:52 +0530

    pushed atanu update homescreen with user data

--- lib/controllers/EditProfileController/EditProfileController.dart
+++ lib/controllers/EditProfileController/EditProfileController.dart
@@ -15,44 +15,36 @@ import 'package:restuarant_pager_app/utils/imagePicker.dart';
 class EditProfileController extends GetxController {
   var model = EditProfileModel().obs;
   final userController = Get.find<UserController>();
-  PhoneNumberController phoneNumberController =
-      Get.put(PhoneNumberController());
+  PhoneNumberController phoneNumberController = Get.put(PhoneNumberController());
   EmailController emailController = Get.put(EmailController(), permanent: true);
-  File? _selectedPic;
+  var _selectedPic = Rx<File?>(null);  // Make _selectedPic observable
 
   @override
   void onInit() {
     super.onInit();
     emailController.emailAddress = userController.email;
-     WidgetsBinding.instance.addPostFrameCallback((_){
-        phoneNumberController.phoneNumberModel.update((model) {
-            model?.phoneNumber =
-                userController.currentUser.value.phone!.phoneNumber;
-            model?.countryCode =
-                userController.currentUser.value.phone!.countryCode;
-        });
-     });
-    
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      phoneNumberController.phoneNumberModel.update((model) {
+        model?.phoneNumber = userController.currentUser.value.phone!.phoneNumber;
+        model?.countryCode = userController.currentUser.value.phone!.countryCode;
+      });
+    });
+
     model.update((model) {
       model?.dateOfBirth = userController.dateOfBirth;
       model?.name = userController.name;
       model?.gender = userController.gender;
       model?.profilePic = userController.profilePic;
-      // model?.email = userController.email;
       model?.phoneNumber = userController.currentUser.value.phone;
     });
   }
 
-
   // Getters
   String? get profilePic => model.value.profilePic;
-  File? get selectedPic => _selectedPic;
+  File? get selectedPic => _selectedPic.value;  // Updated to return observable value
   String? get gender => model.value.gender;
   String? get dateOfBirth => model.value.dateOfBirth;
   String? get name => model.value.name;
-  // String? get emailAddress => model.value.email;
-  // String? get phoneNumber => model.value.phoneNumber?.phoneNumber;
-  // String? get countryCode => model.value.phoneNumber!.countryCode;
   String? get emailAddress => emailController.emailAddress;
   String? get phoneNumber => phoneNumberController.phoneNumber;
   String? get countryCode => phoneNumberController.selectedCountryCode;
@@ -68,15 +60,15 @@ class EditProfileController extends GetxController {
 
   Future<void> selectImage() async {
     final file = await pickImage(ImageSource.gallery);
-    _selectedPic = file;
+    _selectedPic.value = file;  // Update the observable
   }
 
   void submit() async {
     final authMethods = Get.find<AuthMethods>();
     // upload profile pic if given
     String? downloadUrl;
-    if (_selectedPic != null) {
-      final res = await StorageMethods().uploadProfilePic(file: _selectedPic!);
+    if (_selectedPic.value != null) {  // Check observable value
+      final res = await StorageMethods().uploadProfilePic(file: _selectedPic.value!);
       if (res.message == "success") {
         downloadUrl = res.data;
       } else {
@@ -106,7 +98,7 @@ class EditProfileController extends GetxController {
   String? validateName() {
     if (model.value.name == null || model.value.name!.isEmpty) {
       return "Name can't be empty";
-    }else if(model.value.name!.contains(RegExp(r'\s'))){
+    } else if (model.value.name!.contains(RegExp(r'\s'))) {
       return "Name can't contain spaces";
     }
     return null;
